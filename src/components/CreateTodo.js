6cda4abed667d3343feceb54b1e56299c71bcdea
Chris <chris.carson@moesol.com>
2019-01-16 14:38:27 -0800

    OnChange

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CreateTodo = ({ value, onClick }) =>
+const CreateTodo = ({ value, onClick , onChange}) =>
 <>
 <label>Enter Task: &nbsp;
-<input type="text" name="task" value={value} />
+<input type="text" name="task" value={value} onChange={evt => onChange(evt.target.value)}/>
 </label>
 <input type="submit" value="Submit" onClick={onClick}/>
 </>
