3bbf15175f2cc1849e13c2766fe2a655a1729bf4
Chris <chris.carson@moesol.com>
2019-01-16 15:12:10 -0800

    passing object

--- src/containers/createTodo.js
+++ src/containers/createTodo.js
@@ -5,6 +5,9 @@ import * as actions from '../actions';
 
 const mapStateToProps = state => ({ value: state.createTodo.value });
 
-const mapDispatchToProps = dispatch => value => dispatch(actions.createTodoUpdated(value), actions.buttonClicked());
+const mapDispatchToProps = dispatch => ({
+        value: value => dispatch(actions.createTodoUpdated(value)),
+        onClick:  actions.buttonClicked()
+    });
 
 export const CreateTodoContainer = connect(mapStateToProps, mapDispatchToProps)(CreateTodo);
