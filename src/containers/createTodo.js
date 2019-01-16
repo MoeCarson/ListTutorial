fa79fa806df854a4f150336c4edb44dbe2c671e9
Chris <chris.carson@moesol.com>
2019-01-16 13:33:15 -0800

    created creaeTodo.js

---
+++ src/containers/createTodo.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = state => ({ return obj });
+
+const mapDispatchToProps = dispatch => ({ return function });
+
+export const CreateTodoContainer = connect(mapStateToProps, mapDispatchToprops)(CreateTodo);
