82c96685a1cf4f3ee49531d30086350b09628925
Chris <chris.carson@moesol.com>
2019-01-16 14:59:07 -0800

    ActionCreators

--- src/containers/createTodo.js
+++ src/containers/createTodo.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
 import CreateTodo from '../components/CreateTodo';
+import * as actions from '../actions';
 
 
 const mapStateToProps = state => ({ value: state.createTodo.value });
 
-const mapDispatchToProps = dispatch => ({ action: "value" });
+const mapDispatchToProps = dispatch => value => dispatch(actions.createTodoUpdated(value), actions.buttonClicked());
 
 export const CreateTodoContainer = connect(mapStateToProps, mapDispatchToProps)(CreateTodo);
