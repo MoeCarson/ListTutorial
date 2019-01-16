f39b5ae8e641968e2fd0c813a92936ba7c1637cc
Chris <chris.carson@moesol.com>
2019-01-15 16:09:39 -0800

    Now have a functional component!

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import logo from './todo.jpg';
 import './App.css';
 
-const List = ["Learn React", "Learn Redux", "Unlearn Lots"];
+const listArray = ["Learn React", "Learn Redux", "Unlearn Lots"];
 
-const listItems = List.map((item) =><li>{item}</li>);
+const TODOList = ({ list }) => list.map((item) => <li>{item}</li>)
 
-const App = ({ name = "default" }) => 
+const App = ({}) => 
       <div className="App">
         <header className="App-header">
           <img src={logo} alt="logo" />
           <p>
             My Important TODO List
           </p>
-          <ul>{listItems}</ul>
+          <TODOList list={listArray}/>
         </header>
       </div>
 
 export default App;
-
-
