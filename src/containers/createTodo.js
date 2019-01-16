107ab7f9e831528bc8800df96095911dce89401a
Chris <chris.carson@moesol.com>
2019-01-16 14:05:07 -0800

    state

--- src/containers/createTodo.js
+++ src/containers/createTodo.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux';
 import CreateTodo from '../components/CreateTodo';
 
 
-const mapStateToProps = state => ({ return obj });
+const mapStateToProps = state => ({ value: state.createTodo.value });
 
-const mapDispatchToProps = dispatch => ({ return function });
+const mapDispatchToProps = dispatch => ({ dispatch => });
 
 export const CreateTodoContainer = connect(mapStateToProps, mapDispatchToProps)(CreateTodo);
