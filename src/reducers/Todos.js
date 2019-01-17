11a5204aab55de6c9bbdec0ba293b5aef9c8438e
Chris <chris.carson@moesol.com>
2019-01-17 09:00:57 -0800

    angry about the spread operator

--- src/reducers/Todos.js
+++ src/reducers/Todos.js
@@ -1,11 +1,12 @@
 import * as consts from '../consts.js'
+import { Todos } from './index.js';
 
 export const initState = ({ todos: [] })
 
 export const reducer = (state = initState, action) => {
     switch (action.type) {
         case consts.BUTTON_CLICKED:
-        return {...state, todos: [ { name: action.payload, done: false }]}
+        return {...state, todos: [... state.Todos.todos, { name: action.payload, done: false }]}
       default:
         return state
     }
