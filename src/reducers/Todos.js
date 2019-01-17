afd931789d3f7463583273e88a8ad880290f0d96
Chris <chris.carson@moesol.com>
2019-01-17 15:22:43 -0800

    WORKING CHECKBOXES

--- src/reducers/Todos.js
+++ src/reducers/Todos.js
@@ -8,11 +8,13 @@ export const reducer = (state = initState, action) => {
             return {...state, todos: [...state.todos, { name: action.payload, done: false }]}
         case consts.CHECK_BOX_UPDATED:
             //return {...state, todos: [...state.todos, { name: action.payload, done: action.payload }]}
-            return {...state, todos: state.todos.map(todo => todo.name === action.name ?
+            
+            return {...state, todos: state.todos.map(todo => todo.name === action.payload.name ?
                     // transform the one with a matching id
-                    { ...todo, done: action.checked }:
+                    { ...todo,  done: action.payload.checked }:
                     // otherwise return original todo
-                    state.todos)}
+                    todo)}
+                    
         default:
             return state
     }
