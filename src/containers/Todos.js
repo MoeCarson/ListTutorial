5ec184e86d0008c3137a51e81a602010041607a5
Chris <chris.carson@moesol.com>
2019-01-17 16:17:06 -0800

    show done checkbox working

--- src/containers/Todos.js
+++ src/containers/Todos.js
@@ -2,12 +2,11 @@ import { connect } from 'react-redux';
 import Todos from '../components/Todos';
 import * as actions from '../actions';
 
-const mapStateToProps = state => ({ todos: state.Todos.todos, showDone: state.Todos.showDone });
+const mapStateToProps = state => ({ todos: (!state.Todos.showDone) ? state.Todos.todos.filter(x=>x.done === false) : state.Todos.todos, showDone: state.Todos.showDone });
 
 const mapDispatchToProps = dispatch => ({
     onChangeCheckBox: value => dispatch(actions.createCeckBoxUpdated(value)),
     onChangeShowDone: value => dispatch(actions.actionShowDone(value)),
 });
-
 
 export const TodosContainer = connect(mapStateToProps, mapDispatchToProps)(Todos);
