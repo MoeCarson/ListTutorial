06965f5048bd5076db4e04d736e732702fcd5190
Chris <chris.carson@moesol.com>
2019-01-18 08:36:07 -0800

    Reverting

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './todo.jpg';
-import './App.css';
 import { CreateTodoContainer } from '../containers/createTodo.js';
 import { TodosContainer } from '../containers/Todos.js';
 
 
 const App = () => 
       <div className="App">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
         <header className="App-header">
           <img src={logo} alt="logo" />
           <p>
