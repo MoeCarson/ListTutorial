991fb22bd7229f21f82e4a84802862101883a50b
Chris <chris.carson@moesol.com>
2019-01-17 16:51:32 -0800

    unkown error

--- src/containers/Todos.js
+++ src/containers/Todos.js
@@ -1,16 +1,14 @@
 import { connect } from 'react-redux';
 import Todos from '../components/Todos';
 import * as actions from '../actions';
-import { createSelector } from 'reselect'
+import { createSelector } from 'reselect';
 
 const selectShowDone = state => state.todos.showDone;
 const selectTodos = state => state.todos.todos;
 
-const getTodos = createSelector(selectShowDone, selectTodos, (!selectShowDone) ? selectTodos.filter(x=>x.done === false) : selectTodos) => 
+const getTodos = createSelector(selectShowDone, selectTodos, (showDone, todos) => (!showDone) ? todos.filter(x=>x.done === false) : todos);
 
-
-
-const mapStateToProps = state => ({ todos: (!state.Todos.showDone) ? state.Todos.todos.filter(x=>x.done === false) : state.Todos.todos, showDone: state.Todos.showDone });
+const mapStateToProps = state => ({ todos: getTodos(state), showDone: state.Todos.showDone });
 
 const mapDispatchToProps = dispatch => ({
     onChangeCheckBox: value => dispatch(actions.createCeckBoxUpdated(value)),
