02a0910392dde3f63e09f9cd29d70373463afbb3
Chris <chris.carson@moesol.com>
2019-01-18 09:48:14 -0800

    Bug fixed

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import { Button, FormGroup, FormControl } from 'react-bootstrap';
 
 const CreateTodo = ({ value, onChange, onClick , disableButton }) =>
 <>
 <FormGroup controlId="formControlsTextarea">
-<FormControl componentClass="textarea" placeholder="Enter Task Here" onChange={evt => onChange(evt.target.value)}/>
+<FormControl componentClass="textarea" placeholder="Enter Task Here" value={value} onChange={evt => onChange(evt.target.value)}/>
 </FormGroup>
 <Button bsStyle="primary" disabled={disableButton} onClick={() => onClick(value)}>Submit</Button>
 </>
