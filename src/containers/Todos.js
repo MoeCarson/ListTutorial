81c3858f89e4fe160b2690783faf584d0eeb3108
Chris <chris.carson@moesol.com>
2019-01-17 16:39:47 -0800

    selector

--- src/containers/Todos.js
+++ src/containers/Todos.js
@@ -1,6 +1,14 @@
 import { connect } from 'react-redux';
 import Todos from '../components/Todos';
 import * as actions from '../actions';
+import { createSelector } from 'reselect'
+
+const selectShowDone = state => state.todos.showDone;
+const selectTodos = state => state.todos.todos;
+
+const getTodos = createSelector(selectShowDone, selectTodos, (!selectShowDone) ? selectTodos.filter(x=>x.done === false) : selectTodos) => 
+
+
 
 const mapStateToProps = state => ({ todos: (!state.Todos.showDone) ? state.Todos.todos.filter(x=>x.done === false) : state.Todos.todos, showDone: state.Todos.showDone });
 
