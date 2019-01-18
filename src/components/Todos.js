0165b390dbeb0ce19c0f05d5cc3dfa1183e51fb9
Chris <chris.carson@moesol.com>
2019-01-18 10:04:44 -0800

    tag type Well now working

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { ListGroup, ListGroupItem, Well } from 'react-bootstrap';
 
 const Todos = ({ todos, onChangeCheckBox, showDone, onChangeShowDone }) => 
-<>
+<><br></br>
 <Well>
 <input type="checkbox" name="showDone" value="test" checked={showDone} onChange={evt => onChangeShowDone( evt.target.checked )}></input>
-&nbsp; Show Completed Todos</Well>
+&nbsp; Show Completed Todos
+</Well><br></br>
 <ListGroup>
-
 {todos.map(item => (
-
 <ListGroupItem key={item.name}>{item.name}
 &nbsp; &nbsp; 
 <input type="checkbox" name={item.name} value="test" checked={item.done} onChange={evt => onChangeCheckBox({ checked: evt.target.checked, name: item.name })}></input>
-
 </ListGroupItem>
 ))}
 </ListGroup>
