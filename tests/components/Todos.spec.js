a68620b81d40e5816fa3aae61f57fe55ccc136db
Chris <chris.carson@moesol.com>
2019-01-23 11:49:20 -0800

    100% coverage on Todo component - was stuck for a while as the tests were interferring with one another.

--- tests/components/Todos.spec.js
+++ tests/components/Todos.spec.js
@@ -4,14 +4,17 @@ import { Provider } from 'react-redux'
 import createStore from '../../src/store'
 import { TodosContainer } from '../../src/containers/Todos'
 import expect from 'expect'
+import * as actions from '../../src/actions/index.js'
 
 const store = createStore()
+store.dispatch(actions.buttonClicked('Test Name'))
+const Todos = mount(<Provider store={store}><TodosContainer /></Provider>)
 
 
 describe('show done checkbox', () => {
 
     it('the value should change when clicked', () => {
-        const Todos = mount(<Provider store={store}><TodosContainer /></Provider>)
+
 
         const checkbox = Todos.find('[data-show-done-todos]').find('input')
         expect(checkbox.prop('checked')).toEqual(true)
@@ -22,5 +25,21 @@ describe('show done checkbox', () => {
             Todos.find('[data-show-done-todos]'
             ).find('input').prop('checked')).toEqual(false)
 
+        checkbox.simulate('change', { target: { checked: true }})
+
+    })
+})
+
+
+describe('individual tasks completed checkboxes', () => {
+
+    it('the value should change when clicked', () => {
+
+        const checkbox2 = Todos.find({ name: 'Test Name' }).find('input')
+        expect(checkbox2.prop('checked')).toEqual(false)
+
+        checkbox2.simulate('change', { target: { checked: true }})
+
+        expect(Todos.find({ name: 'Test Name' }).find('input').prop('checked')).toEqual(true)
     })
 })
