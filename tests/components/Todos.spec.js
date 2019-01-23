b6f8902e5ccb3c12f0662b2b7146fdece5681c7f
jtaylor <j.taylor@moesol.com>
2019-01-23 10:27:51 -0800

    fix for first enzyme test

--- tests/components/Todos.spec.js
+++ tests/components/Todos.spec.js
@@ -1,31 +1,26 @@
 import { mount } from 'enzyme'
 import React from 'react'
-//import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import createStore from '../../src/store'
 import { TodosContainer } from '../../src/containers/Todos'
 import expect from 'expect'
 
-//const testState = {'showDone': false, 'todos': [{'done': false, 'name': 'Test Name'}]}
-
 const store = createStore()
 
-//describe('containers/Todos.js', () => {
 
 describe('show done checkbox', () => {
 
     it('the value should change when clicked', () => {
         const Todos = mount(<Provider store={store}><TodosContainer /></Provider>)
-        //const checkbox = showdone.find({ name: 'showDone', type: 'checkbox' }).at(1)
 
-        //expect(checkbox.props().checked).toEqual(true)
-        expect(Todos.find('[id="showDoneUnique"]').props()).toEqual(true)
+        const checkbox = Todos.find('[data-show-done-todos]').find('input')
+        expect(checkbox.prop('checked')).toEqual(true)
 
-        //showdone.setProps(testState)
-        //checkbox.simulate('click')
-        //expect(showdone.find({ name: 'showDone', type: 'checkbox' }).at(1).props().checked).toEqual(false)
-        //expect(showdone.props()).toEqual(false)
+        checkbox.simulate('change', { target: { checked: false }})
+
+        expect(
+            Todos.find('[data-show-done-todos]'
+            ).find('input').prop('checked')).toEqual(false)
+
     })
 })
-
-//})
