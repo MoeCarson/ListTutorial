ac18045cba57841254995a7e0a4d18698e56dd1d
Chris <chris.carson@moesol.com>
2019-01-24 15:54:05 -0800

    Attempting to add type for each reducer's store and over-all redux shape

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,4 +2,13 @@ type Action<T> = { type: string, payload: T }
 
 type ActionCreator<T> = (payload: T) => Action<T>
 
-type Reducer<P, T> = (state: P, Action: T) => state
+type Reducer<P> = (state: P, action: Action<any>) => P
+
+// Type for state for reducer Todo
+type Todos = ({ todos: Array, showDone: Boolean})
+
+// Type for state for reducer createTodo
+type createTodo = ({ value: string })
+
+// Type for over-all shape of Redux
+type OverallReduxShape = (Todos: Todos, createTodo: createTodo) => { Todos: Todos, createTodo: createTodo }
