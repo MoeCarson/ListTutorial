5dde96e4af19a4514df5d9fca63ab47c28e85d0d
Chris <chris.carson@moesol.com>
2019-01-24 17:32:45 -0800

    created production files

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+/* global module */
+const path = require('path')
+const merge = require('webpack-merge')
+//const webpack = require('webpack')
+const common = require('./webpack.common.js').config
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: '[hash]-bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+})
+
